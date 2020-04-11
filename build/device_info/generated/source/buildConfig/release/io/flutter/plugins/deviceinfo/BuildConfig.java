6aded6837faf29ad50031127ad5a05142cd0a538
Venkat <venkatesh@eventova.com>
2020-04-11 16:57:38 +0530

    package name change

--- build/device_info/generated/source/buildConfig/release/io/flutter/plugins/deviceinfo/BuildConfig.java
+++ build/device_info/generated/source/buildConfig/release/io/flutter/plugins/deviceinfo/BuildConfig.java
@@ -5,6 +5,11 @@ package io.flutter.plugins.deviceinfo;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "io.flutter.plugins.deviceinfo";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "io.flutter.plugins.deviceinfo";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
